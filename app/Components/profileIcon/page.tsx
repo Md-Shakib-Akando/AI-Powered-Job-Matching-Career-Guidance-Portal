171bf69d9feef950415092dde21c3a1a7780e256
Md-Shakib-Akando <shakib.akando.dev@gmail.com>
2026-01-08 23:55:50 +0600

    modify profile menu

--- app/Components/profileIcon/page.tsx
+++ app/Components/profileIcon/page.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { FaUserCircle } from "react-icons/fa"
 import { Session } from "next-auth"
+import Link from "next/link"
 
 interface ProfileMenuProps {
     session: Session | null
@@ -20,34 +21,53 @@ const ProfileMenu = ({ session, handleLogout }: ProfileMenuProps) => {
             </button>
 
             {open && (
-                <div className="absolute -right-18 mt-3 w-44 bg-[#124271] border border-white/20 rounded-xl shadow-lg z-50 overflow-hidden">
-                    <p className="px-4 py-3 font-medium text-center text-white border-b border-white/20">
-                        Name : {session?.user?.name}
-                    </p>
+                <div
+                    className="
+            absolute -right-18 mt-3 w-52
+            bg-[#124271]
+            border border-white/20
+            rounded-xl
+            shadow-2xl
+            z-50
+            overflow-hidden
+        "
+                >
 
-                    <button
-                        onClick={handleLogout}
-                        className="
-                w-full
-                px-4
-                py-2.5
-                bg-red-500
-                text-white
-                font-semibold
-                rounded-b-xl
-                hover:bg-red-600
-                active:scale-95
-                transition-all
-                duration-200
-                focus:outline-none
-                focus:ring-2
-                focus:ring-red-400
-            "
-                    >
-                        Logout
-                    </button>
+                    <div className="px-4 py-3 border-b border-white/20">
+                        <p className="text-lg text-white/70 text-center">Name : {session?.user?.name}</p>
+
+                    </div>
+
+
+                    <div className="py-1">
+                        <Link
+                            href="/dashboard"
+                            className="
+                    flex items-center justify-center
+                    px-4 py-2.5
+                    text-sm font-medium
+                    text-white
+                    hover:bg-white/10
+                    transition
+                "
+                        >
+                            Dashboard
+                        </Link>
+                    </div>
+
+
+                    <div className="w-full flex items-center justify-center px-4 py-3 border-t border-white/20">
+                        <button
+                            onClick={handleLogout}
+                            className="
+                 px-5 py-2  bg-red-500  text-white  font-semibold  hover:bg-red-600  active:scale-95  transition-all  duration-200  focus:outline-none  focus:ring-2  focus:ring-red-40 rounded "
+                        >
+                            Logout
+                        </button>
+                    </div>
                 </div>
             )}
+
 
         </div>
     )
